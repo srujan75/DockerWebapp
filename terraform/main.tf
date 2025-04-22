1771cbc81c16d06feb735c06a84f2cb9d73bd13d
srujan75 <srujanneeruganti@gmail.com>
2025-04-22 16:36:00 +0530

    Make changes in code

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,30 +10,30 @@ terraform {
 provider "null" {}
 
 variable "render_api_key" {
-  description = "API key for Render"
+  description = "Render API key"
   type        = string
 }
 
-resource "null_resource" "create_render_service" {
+resource "null_resource" "deploy_to_render" {
   provisioner "local-exec" {
     command = <<EOT
       curl -X POST https://api.render.com/v1/services \
-      -H "Authorization: Bearer ${var.render_api_key}" \
-      -H "Content-Type: application/json" \
-      -d '{
-        "type": "web_service",
-        "name": "fastapi-app",
-        "env": "docker",
-        "repo": {
-          "url": "https://github.com/srujan75/webapp-docker-terraform-render"
-        },
-        "branch": "main",
-        "plan": "free",
-        "region": "oregon",
-        "dockerContext": ".",
-        "dockerfilePath": "Dockerfile"
-      }'
+        -H "Authorization: Bearer ${var.render_api_key}" \
+        -H "Content-Type: application/json" \
+        -d '{
+          "type": "web_service",
+          "name": "fastapi-app",
+          "env": "docker",
+          "repo": {
+            "url": "https://github.com/srujan75/DockerWebapp"
+          },
+          "branch": "main",
+          "plan": "free",
+          "region": "oregon",
+          "dockerContext": ".",
+          "dockerfilePath": "Dockerfile"
+        }'
     EOT
     interpreter = ["/bin/bash", "-c"]
-   }
+  }
 }
